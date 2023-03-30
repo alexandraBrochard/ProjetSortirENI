e64dc69d7536ab5067fabc5fba4a6d7274a5c8e9
blambertt <lambertraverdy@gmail.com>
2023-03-30 09:36:59 +0200

    minor fixes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,4 +18,30 @@ class AdminController extends AbstractController
 
         ]);
     }
+    #[Route('/admin/villes', name: 'admiin_villes')]
+    public function villes(): Response
+    {
+
+        return $this->render('admin/villes.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/admin/campus', name: 'admin_campus')]
+    public function campus(): Response
+    {
+
+        return $this->render('admin/campus.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/admin/utilisateurs', name: 'admin_utilisateurs')]
+    public function utilisateurs(): Response
+    {
+
+        return $this->render('admin/utilisateurs.html.twig', [
+
+        ]);
+    }
 }
